59aca554d3b55931f163da55a1339014e46e5302
Sarath Thatavarthy <sarathchandra.mail2me@gmail.com>
2024-01-09 16:17:44 +0000

    Adding storage buckets

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-# resource "google_storage_bucket" "bucket" {
-#   name     = "test-bucket-random-001122-221"
-#   location = "europe-west2"
-# }
+resource "google_storage_bucket" "bucket" {
+  name     = "test-bucket-random-001122-221"
+  location = "europe-west2"
+}
 
-# resource "google_storage_bucket" "gcs_bucket" {
-#   name     = "test-bucket-random-001123231"
-#   location = "europe-west2"
-# }
+resource "google_storage_bucket" "gcs_bucket" {
+  name     = "test-bucket-random-001123231"
+  location = "europe-west2"
+}
