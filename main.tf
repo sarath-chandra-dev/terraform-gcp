103f7907268f99d50dc50b5d755743bb055f23de
Sarath Thatavarthy <sarathchandra.mail2me@gmail.com>
2024-01-09 05:15:09 +0000

    Adding apply to the terraform

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
 # Triggering the build 11
 resource "google_storage_bucket" "bucket" {
   name     = "test-bucket-random-001122-22"
-  location = "europe-west2"
+  project = "the-experience-270205"
+  location = "eastus2"
 }
 
 resource "google_storage_bucket" "gcs_bucket" {
   name     = "test-bucket-random-00112323"
-  location = "europe-west2"
+  location = "eastus2"
+  project = "the-experience-270205"
 }
