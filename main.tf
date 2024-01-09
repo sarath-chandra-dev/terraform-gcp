e92e5cf225a6b3ba9caba28433b04b7236ae0a97
Sarath Thatavarthy <sarathchandra.mail2me@gmail.com>
2024-01-09 16:28:22 +0000

    Adding backend

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+# Configure the backend to use the Terraform Cloud remote backend.
+terraform {
+  cloud {
+    organization = "Sarath-GCP"
+
+    workspaces {
+      name = "gcp-api-driven-workflow"
+    }
+  }
+}
+
 resource "google_storage_bucket" "bucket" {
   name     = "test-bucket-random-001122-221"
   location = "europe-west2"
