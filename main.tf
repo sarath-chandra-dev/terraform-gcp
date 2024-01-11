331aff72ac391982eaa1a1910494d72f0921fa6d
Sarath Thatavarthy <sarathchandra.mail2me@gmail.com>
2024-01-11 14:51:18 +0000

    Fixing the terraform format

--- main.tf
+++ main.tf
@@ -1,29 +1,3 @@
-# terraform {
-#   required_providers {
-#     google = {
-#       source  = "hashicorp/google"
-#       version = "4.57.0"
-#     }
-#   }
-#   backend "gcs" {
-    
-#   }
-# }
-
-# provider "google" {
-#   project = "the-experience-270205"
-# }
-
-# resource "google_storage_bucket" "bucket" {
-#   name     = "test-bucket-random-001122-221"
-#   location = "europe-west2"
-# }
-
-# resource "google_storage_bucket" "gcs_bucket" {
-#   name     = "test-bucket-random-001123231"
-#   location = "europe-west2"
-# }
-
 # The configuration for the `remote` backend.
 terraform {
   backend "remote" {
